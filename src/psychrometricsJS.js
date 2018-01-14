32b158f4ed0405329ab7bf4870e2e5ae68c036cc
Julien de Charentenay <julien.decharentenay@gmail.com>
2018-01-14 20:26:49 +0000

    Define SI Functions and add dbt/dpt state definition

--- src/psychrometricsJS.js
+++ src/psychrometricsJS.js
@@ -3,7 +3,7 @@
  * Author
  * Julien de Charentenay
  *
- * Released under the MIT License.
+ * Licensed under MIT license
  *
  * v x.x.x
  */
@@ -13,21 +13,14 @@ var PsychrometricsJS = PsychrometricsJS || {};
 PsychrometricsJS.major_version = 0;
 PsychrometricsJS.minor_version = 0;
 PsychrometricsJS.author = "Julien de Charentenay";
-PsychrometricsJS.copyright = "Copyright 2017, Julien de Charentenay";
-PsychrometricsJS.license = "Library license under TBA";
-PsychrometricsJS.website = "http://www.tba.com";
+PsychrometricsJS.copyright = "Copyright 2018, Julien de Charentenay";
+PsychrometricsJS.license = "Licensed under MIT License";
+PsychrometricsJS.website = "http://juliendecharentenay.wordpress.com";
 
-// Gas constant for dry-air
-PsychrometricsJS.RDA = 287.042; // J/kgda.K
-
-// Gas constant for water
-PsychrometricsJS.RW = 461.524; // J/kgw.K
-
-// Gravity constant
-PsychrometricsJS.G = 9.80665; // m/s2
+PsychrometricsJS.TMIN = -100.0;
+PsychrometricsJS.TMAX = 200.0;
 
 PsychrometricsJS.Units = {};
-
 /**
  * Length
  */
@@ -113,10 +106,14 @@ PsychrometricsJS.Temperature.prototype.from_fahrenheit = function(value) { this.
 PsychrometricsJS.Temperature.prototype.to_fahrenheit = function(value) { return 9.0/5.0 * this.value + 32.0; };
 
 /**
- * Functions
+ * SI Functions
  */
 
-PsychrometricsJS.Functions = {};
+PsychrometricsJS.SI = {};
+
+PsychrometricsJS.SI.RDA = 287.042; // Gas constant for dry-air, J/kgda.K
+PsychrometricsJS.SI.RW = 461.524;  // Gas constant for water, J/kgw.K
+PsychrometricsJS.SI.G = 9.80665;   // Gravity constant, m/s2
 
 /**
  * Calculate Water vapor saturation pressure over ice and liquid water
@@ -128,7 +125,7 @@ PsychrometricsJS.Functions = {};
  * return 
  *   p: in pascal
  */
-PsychrometricsJS.Functions.getWaterVaporSaturationPressure = function(t) {
+PsychrometricsJS.SI.getWaterVaporSaturationPressure = function(t) {
   if ((t < -100.0) || (t > 200.0)) {
     throw "Water vaport saturation pressure only valid for temperature between -100.0 and +200.0 Celsius. Temperature provided is: " + t;
   }
@@ -165,10 +162,135 @@ PsychrometricsJS.Functions.getWaterVaporSaturationPressure = function(t) {
  * return 
  *   W: in kg water / kg dry-air
  */
-PsychrometricsJS.Functions.getHumidityRatio = function(p, pw) {
+PsychrometricsJS.SI.getHumidityRatio = function(p, pw) {
   return 0.621945*pw/(p-pw);
 };
 
+/**
+ * Calculate humidity rato from dry-bulb, wet-bulb temperature and pressure
+ * Uses ASHRAE Handbook Fundamentals 2013 
+ * Chapter 1 Psycrhometrics: Equations (35/36)
+ *
+ * input:
+ *  dbt: dry-bulb temperature (C)
+ *  wbt: wet-bulb temperature (C)
+ *  p: pressure (Pa)
+ * return 
+ *   W: in kg water / kg dry-air
+ */
+PsychrometricsJS.SI.getHumidityRatioFromDbtWbt = function(dbt, wbt, p) {
+  var pws_star = PsychrometricsJS.SI.getWaterVaporSaturationPressure(wbt);
+  var ws_star = PsychrometricsJS.SI.getHumidityRatio(p, pws_star);
+  var w = null;
+  if (dbt >= 0.0) {
+    w = ((2501.0 - 2.326*wbt)*ws_star - 1.006*(dbt-wbt))/(2501.0+1.86*dbt-4.186*wbt);
+  } else {
+    w = ((2830.0 - 0.24*wbt)*ws_star - 1.006*(dbt-wbt))/(2830.0+1.86*dbt-2.1*wbt);
+  }
+  return (w >= 1e-5) ? w : 1e-5;
+};
+
+/**
+ * Calculate relative humidity from wet-bulb and dry-bulb temperature
+ * Uses ASHRAE Handbook Fundamentals 2013 
+ *
+ * Input:
+ *  dbt: dry-bulb temperature (C)
+ *  wbt: wet-bulb temperature (C)
+ *  p: pressure (Pa)
+ * return:
+ *   relative humidity: between 0 and 1
+ */
+PsychrometricsJS.SI.getRelativeHumidityFromDbtWbt = function(dbt, wbt, p) {
+  var w = PyschrometricsJS.SI.getHumidityRatioFromDbtWbt(dbt, wbt, p); 
+  return PsychrometricsJS.SI.getRelativeHumidityFromDbtW(dbt, w, p);
+};
+
+/**
+ * Calculate relative humidity from dry-bulb temperature and humidity ratio
+ * Uses ASHRAE Handbook Fundamentals 2013 
+ * Chapter 1 Psycrhometrics: Equations (5), (6), (22), (23), (12), (25)
+ *
+ * Input:
+ *  dbt: dry-bulb temperature (C)
+ *  w: humidity ratio (kg/kgda)
+ *  p: pressure (Pa)
+ * return:
+ *   relative humidity: between 0 and 1
+ */
+PsychrometricsJS.SI.getRelativeHumidityFromDbtW = function(dbt, w, p) {
+  var pws = PsychrometricsJS.SI.getWaterVaporSaturationPressure(dbt);  // eq (5) and (6)
+  var ws = PsychrometricsJS.SI.getHumidityRatio(p, pws);               // eq (22)
+  var mu = w/ws;                                                       // eq (12)
+  return mu / (1.0 - (1.0 - mu)*(pws/p));                              // eq (25)
+};
+
+/**
+ * Calculate dew-point temperature from humidity ratio and pressure
+ * Uses ASHRAE Handbook Fundamentals 2013 
+ * Chapter 1 Psychrometrics: Equations (39/40)
+ *
+ * Input:
+ *  w: humidity ratio (kg/kgda)
+ *  p: pressure (Pa)
+ * return 
+ *  dpt: temperature (C)
+ */
+PsychrometricsJS.SI.getDewPointTemperature = function(w, p) {
+  var pw = p/1000.0*w/(0.621945+w);
+  var alpha = Math.log(pw);
+  var c14 = 6.54;
+  var c15 = 14.526;
+  var c16 = 0.7389;
+  var c17 = 0.09486;
+  var c18 = 0.4569;
+  var dpt = c14+alpha*(c15+alpha*(c16+alpha*c17))+c18*Math.pow(pw, 0.1984);
+  dpt = (dpt >= 0.0) ? dpt : 6.09 + alpha*(12.608 + 0.4959*alpha);
+  return dpt
+};
+
+/**
+ * Calculate enthalpy from dry-bulb temperature and humidity ratio
+ * Uses ASHRAE Handbook Fundamentals 2013 
+ * Chapter 1 Psycrhometrics: Equations (32)
+ *
+ * Input:
+ *  dbt: dry-bulb temperature (C)
+ *  w: humidity ratio (kg/kgda)
+ * return 
+ *   h: enthalpy (kJ/kgda)
+ */
+PsychrometricsJS.SI.getEnthalpy = function(dbt, w) {
+  return 1.006*dbt + w*(2501.0 + 1.86*dbt);
+};
+
+/**
+ * Numerical Solver: Using bisection method
+ */
+PsychrometricsJS.solveBisection = function(vmin, vmax, target, err, func) {
+  var fmin = func(vmin); var fmax = func(vmax);
+  if ((target - fmin)*(target-fmax) > 0) { throw "Bisection solver error: target value " + target + " is not within interval vmin|vmax. Function may not be monotonous"; }
+  if (Math.abs(fmin - target) <= err) { return vmin; }
+  if (Math.abs(fmax - target) <= err) { return vmax; }
+
+  var v = 0.5*(vmin+vmax); var f = func(v);
+  while (Math.abs(f-target)>err) {
+    if ((target-f)*(target-fmin) < 0) { // Target is between vmin and v. Update vmax/fmax
+      vmax = v; fmax = f;
+    } else if ((target-f)*(target-fmax) < 0) { // Target is between v and vmax. Update vmin/fmin
+      vmin = v; fmin = f;
+    } else {
+      throw "Bisection solver error: Can find updated interval";
+    }
+    v = 0.5*(vmin+vmax); f = func(v);
+  };
+  return v;
+};
+
+
+/**
+ * Standard conditions
+ */
 PsychrometricsJS.getStandardAtmPressure = function(z) {
   return new PsychrometricsJS.Pressure(101.325*Math.pow(1.0-2.25577e-5*z.to_meter(),5.2559), PsychrometricsJS.Units.KILOPASCAL); // kPa
 };
@@ -184,7 +306,7 @@ PsychrometricsJS.Variables = {
   WBT: "wet-bulb temperature",
   RH: "relative humidity",
   H: "Enthalpy",
-  HR: "Humidity ratio",
+  W: "Humidity ratio",
   P: "Pressure"
 };
 
@@ -194,16 +316,28 @@ PsychrometricsJS.State = function(hash) {
   }
   if (! hash.hasOwnProperty(PsychrometricsJS.Variables.P)) { throw "Specify pressure"; }
 
+  // Core variables
   this.dbt = null;
-  // this.enthalpy = null;
   this.wbt = null;
   this.p = null;
-  if (hash.hasOwnProperty(PsychrometricsJS.Variables.DBT)
-      && hash.hasOwnProperty(PsychrometricsJS.Variables.WBT)) {
-    this.from_dbtwbt(hash[PsychrometricsJS.Variables.DBT],
-                     hash[PsychrometricsJS.Variables.WBT],
-                     hash[PsychrometricsJS.Variables.P]);
-  } else {
+
+  // Derived variables
+  this.dpt = null;
+  this.h = null;
+  this.w = null;
+  this.rh = null;
+
+  if (hash.hasOwnProperty(PsychrometricsJS.Variables.DBT)) {
+    if (hash.hasOwnProperty(PsychrometricsJS.Variables.WBT)) {
+      this.from_dbtwbt(hash[PsychrometricsJS.Variables.DBT], hash[PsychrometricsJS.Variables.WBT], hash[PsychrometricsJS.Variables.P]);
+
+    } else if (hash.hasOwnProperty(PsychrometricsJS.Variables.DPT)) {
+      this.from_dbtdpt(hash[PsychrometricsJS.Variables.DBT], hash[PsychrometricsJS.Variables.DPT], hash[PsychrometricsJS.Variables.P]);
+    }
+  }
+
+  // Check that core variables are specified
+  if ((this.dbt == null) || (this.wbt == null)) {
     throw "State specification is incomplete or not supported";
   }
 
@@ -216,112 +350,56 @@ PsychrometricsJS.State.prototype.from_dbtwbt = function(dbt, wbt, p) {
   this.p = p;
 };
 
-PsychrometricsJS.State.prototype.from_dbtwbt2 = function(dbt, wbt, p) {
-  var pws_star = PsychrometricsJS.getWaterVaporSaturationPressure(wbt);
-  var ws_star = PsychrometricsJS.getHumidityRatio(p, pws_star);
-  var w = (dbt.to_celsius() < 0) ? ((2830.0 - 0.24*wbt.to_celsius())*ws_star - 1.006*(dbt.to_celsius() - wbt.to_celsius()))/(2830.0+1.86*dbt.to_celsius()-2.1*wbt.to_celsius()) : ((2501 - 2.326*wbt.to_celsius())*ws_star - 1.006*(dbt.to_celsius()-wbt.to_celsius()))/(2501.0+1.86*dbt.to_celsius()-4.186*wbt.to_celsius());
-
-  var pws = PsychrometricsJS.getWaterVaporSaturationPressure(dbt);
-  var ws = PsychrometricsJS.getHumidityRatio(p, pws);
-  var mu = ws/ws_star;
-  var rh = mu / (1.0 - (1.0 - mu)*(pws.to_pascal()/p.to_pascal()));
-  var specificVolume = 0.287042*dbt.to_kelvin()*(1.0 + 1.607858*w)/p.to_kilopascal(); // m3/kg_da
-  var enthalpy = 1.006*dbt.to_celsius() + w*(2501.0 + 1.86*dbt.to_celsius());
-  var pw = p.to_pascal()*w/(0.621945+w);
-  var alpha = Math.log(pw);
-  var dpt = 6.54+14.526*(alpha+0.7389*(alpha+0.7389*(alpha+0.09486*alpha)))+0.4569*Math(pw, 0.1984);
-  dpt = (dpt >= 0.0) ? dpt : 6.09 + 12.608*(alpha + 0.4959*alpha);
-  dpt = PsychrometricsJS.Temperature(dpt, PsychrometricsJS.Units.CELSIUS);
-
+PsychrometricsJS.State.prototype.from_dbtdpt = function(dbt, dpt, p) {
   this.dbt = dbt;
-  this.enthalpy = enthalpy;
+  this.dpt = dpt;
   this.p = p;
+  var wbt = PsychrometricsJS.solveBisection(PsychrometricsJS.TMIN, this.dbt.to_celsius(), this.dpt.to_celsius(), 1e-4,
+                        function(wbt) {
+                          var w = PsychrometricsJS.SI.getHumidityRatioFromDbtWbt(this.dbt, wbt, this.p);
+                          return PsychrometricsJS.SI.getDewPointTemperature(w, this.p);
+                        }.bind({'dbt': this.dbt.to_celsius(), 'p': this.p.to_pascal()}));
+  this.wbt = new PsychrometricsJS.Temperature(wbt, PsychrometricsJS.Units.CELSIUS);
 };
 
-/**
- * Calculate dew-point temperature for state
- * Uses ASHRAE Handbook Fundamentals 2013 
- * Chapter 1 Psycrhometrics: Equations (39/40)
- *
- * return 
- *   dpt: temperature
- */
+
 PsychrometricsJS.State.prototype.getDewPointTemperature = function() {
-  var w = this.getHumidityRatio();
-  var pw = this.p.to_kilopascal()*w/(0.621945+w);
-  var alpha = Math.log(pw);
-  var c14 = 6.54;
-  var c15 = 14.526;
-  var c16 = 0.7389;
-  var c17 = 0.09486;
-  var c18 = 0.4569;
-  var dpt = c14+alpha*(c15+alpha*(c16+alpha*c17))+c18*Math.pow(pw, 0.1984);
-  dpt = (dpt >= 0.0) ? dpt : 6.09 + alpha*(12.608 + 0.4959*alpha);
-  return new PsychrometricsJS.Temperature(dpt, PsychrometricsJS.Units.CELSIUS);
+  if (this.dpt == null) {
+    var dpt = PsychrometricsJS.SI.getDewPointTemperature(this.getHumidityRatio(), this.p.to_pascal());
+    this.dpt = new PsychrometricsJS.Temperature(dpt, PsychrometricsJS.Units.CELSIUS);
+  }
+  return this.dpt;
 };
 
 PsychrometricsJS.State.prototype.getDryBulbTemperature = function() {
   return this.dbt;
 };
 
-/**
- * Calculate humidity rato for state
- * Uses ASHRAE Handbook Fundamentals 2013 
- * Chapter 1 Psycrhometrics: Equations (32)
- *
- * return 
- *   h: in kJ / kg dry-air
- */
 PsychrometricsJS.State.prototype.getEnthalpy = function() {
-  var w = this.getHumidityRatio();
-  return 1.006*this.dbt.to_celsius() + w*(2501.0 + 1.86*this.dbt.to_celsius());
+  if (this.h == null) {
+    var h = PsychrometricsJS.SI.getEnthalpy(this.dbt.to_celsius(), this.getHumidityRatio());
+    this.h = h;
+  }
+  return this.h;
 };
 
 PsychrometricsJS.State.prototype.getWetBulbTemperature = function() {
   return this.wbt;
 };
 
-/**
- * Calculate humidity rato for state
- * Uses ASHRAE Handbook Fundamentals 2013 
- * Chapter 1 Psycrhometrics: Equations (35/36)
- *
- * return 
- *   W: in kg water / kg dry-air
- */
 PsychrometricsJS.State.prototype.getHumidityRatio = function() {
-  var pws_star = PsychrometricsJS.Functions.getWaterVaporSaturationPressure(this.wbt.to_celsius());
-  var ws_star = PsychrometricsJS.Functions.getHumidityRatio(this.p.to_pascal(), pws_star);
-  if (this.dbt.to_celsius() >= 0.0) {
-    return ((2501.0 - 2.326*this.wbt.to_celsius())*ws_star - 1.006*(this.dbt.to_celsius()-this.wbt.to_celsius()))/(2501.0+1.86*this.dbt.to_celsius()-4.186*this.wbt.to_celsius());
-  } else {
-    return ((2830.0 - 0.24*this.wbt.to_celsius())*ws_star - 1.006*(this.dbt.to_celsius() - this.wbt.to_celsius()))/(2830.0+1.86*this.dbt.to_celsius()-2.1*this.wbt.to_celsius());
+  if (this.w == null) {
+    var w = PsychrometricsJS.SI.getHumidityRatioFromDbtWbt(this.dbt.to_celsius(), this.wbt.to_celsius(), this.p.to_pascal());
+    this.w = w;
   }
+  return this.w;
 };
 
-/**
- * Calculate relative humidity from wet-bulb and dry-bulb temperature
- * Uses ASHRAE Handbook Fundamentals 2013 
- * Chapter 1 Psycrhometrics: Equations (5), (6), (22), (23), (12), (25)
- *
- * return 
- *   relative humidity: between 0 and 1
- */
 PsychrometricsJS.State.prototype.getRelativeHumidity = function() {
-  var w = this.getHumidityRatio();
-  var pws = PsychrometricsJS.Functions.getWaterVaporSaturationPressure(this.dbt.to_celsius());      // eq (5) and (6)
-  var ws = PsychrometricsJS.Functions.getHumidityRatio(this.p.to_pascal(), pws);                    // eq (22)
-  var mu = w/ws;                                                                                    // eq (12)
-  return mu / (1.0 - (1.0 - mu)*(pws/this.p.to_pascal()));                                          // eq (25)
+  if (this.rh == null) {
+    var rh = PsychrometricsJS.SI.getRelativeHumidityFromDbtW(this.dbt.to_celsius(), this.getHumidityRatio(), this.p.to_pascal());
+    this.rh = rh;
+  }
+  return this.rh;
 };
 
-/**
-PsychrometricsJS.State.prototype.getDewPointTemperature = function() {
-  var w = this.getHumidityRatio();
-  var pw = this.p.to_pascal()*w/(0.621945+w);
-  var alpha = Math.log(pw);
-  var dpt = 6.54+14.526*(alpha+0.7389*(alpha+0.7389*(alpha+0.09486*alpha)))+0.4569*Math(pw, 0.1984);
-  dpt = (dpt >= 0.0) ? dpt : 6.09 + 12.608*(alpha + 0.4959*alpha);
-  return new PsychrometricsJS.Temperature(dpt, PsychrometricsJS.Units.CELSIUS);
-};
-*/
